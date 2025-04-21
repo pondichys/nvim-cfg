c97980043500c191056b7349384c75e37cc85905
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-04-21 17:33:22 +0200

    feat: add mini.pairs and mini.hipatterns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -62,5 +62,40 @@ return {
       },
     },
   },
+  -- Auto pair
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    -- opts shamelessly stolen from LazyVim configuration
+    opts = {
+      modes = { insert = true, command = false, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = true,
+      -- better deal with markdown code blocks
+      markdown = true,
+    },
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    version = false,
+    config = function()
+      require('mini.hipatterns').setup({
+        highlighters = {
+          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+          hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+          todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+          note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
 
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = require('mini.hipatterns').gen_highlighter.hex_color(),
+        },
+      })
+    end
+  },
 }
