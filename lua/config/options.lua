f00b23a0d6713dc0f03fcad421921bfa5122a91e
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-08-21 11:31:27 +0200

    feat: add options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,36 +1,43 @@
 local opt = vim.opt
 
--- Tabs
+-- Search
+opt.grepprg = "rg --vimgrep" -- Use ripgrep if available
+opt.grepformat = "%f:%l:%c:%m" -- filename, linenumber, column, content
+opt.hlsearch = true -- Highlight search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Tabs / indentation
+opt.autoindent = true -- Copy indent from current line
 opt.expandtab = true -- Convert tabs to spaces
 opt.shiftwidth = 4   -- Amount to indent with << and >>
 opt.tabstop = 4      -- How many spaces are shown per Tab
 opt.softtabstop = 4  -- How many spaces are applied when pressing Tab
 
--- Display relative line numbers
+-- UI
+opt.cursorline = true
 opt.number = true
 opt.relativenumber = true
-
--- Display cursor line
-opt.cursorline = true
-
--- Enable mouse support
-opt.mouse = "a"
-
--- Do not show mode as it is already in the statusline
 opt.showmode = false
+opt.signcolumn = "yes"
+opt.winborder = "rounded"  -- Use rounded window borders
 
+-- Behavior
+opt.clipboard:append("unnamedplus")
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 opt.list = true
 opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
+opt.mouse = "a" -- Enable mouse support
+opt.scrolloff = 10 -- Keep 10 lines above/below cursor
+opt.spelllang = { "en", "fr" } -- Set languages for spellchecking
+
 -- Define sane default for splits
 opt.splitright = true
 opt.splitbelow = true
-
--- Use rounded window borders
-opt.winborder = "rounded"
 
 --
 -- Diagnostic options
